8dad13a1082c6f9e49bdcbe6543be70b67a5a98b
TarekSaleh99 <tareksaleh2000a@gmail.com>
2025-12-13 19:32:14 +0200

    register models in admin and migrate

--- backend/profiles/admin.py
+++ backend/profiles/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import UserProfile, UserHealthData
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "display_name",
+        "preferred_currency",
+        "sex",
+        "activity_level",
+        "goal",
+        "last_active",
+    )
+    search_fields = ("user__email", "display_name")
+    list_filter = ("sex", "activity_level", "goal", "preferred_currency")
+    ordering = ("id",)
+    readonly_fields = ("last_active",)
+
+
+@admin.register(UserHealthData)
+class UserHealthDataAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "dietary_preferences",
+        "allergies",
+        "medical_conditions",
+    )
+    search_fields = ("user__email",)
+    ordering = ("id",)
