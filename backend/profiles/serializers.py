ef77caa6614403f4fe11409cfb06bdfb10c9284f
youssef-M-hassan <youssef.m.hassan56@gmail.com>
2025-12-27 04:12:40 +0200

    MealLogs

--- backend/profiles/serializers.py
+++ backend/profiles/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import UserProfile, UserHealthData
+from .models import UserProfile, UserHealthData, MealLog
+from foods.serializers import MealSerializer
+from foods.models import Meal
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -15,4 +17,18 @@ class UserHealthDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserHealthData
         fields = "__all__"
+        read_only_fields = ("user",)
+
+
+class MealLogSerializer(serializers.ModelSerializer):
+    meal_id = serializers.PrimaryKeyRelatedField(
+        queryset=Meal.objects.all(),
+        source="meal",
+        write_only=True,
+    )
+    meal = MealSerializer(read_only=True)
+
+    class Meta:
+        model = MealLog
+        fields = ("id", "meal", "consumed_at", "meal_id")
         read_only_fields = ("user",)
