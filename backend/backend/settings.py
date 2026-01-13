a52e7a0e7bedc936b49a8559e2df22b5225c3ab6
TarekSaleh99 <tareksaleh2000a@gmail.com>
2026-01-13 19:42:08 +0200

    Fix Docker setup for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
